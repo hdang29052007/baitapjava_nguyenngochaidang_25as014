d9050611d58d5f1243f58947a0efe9a7a7ce2642
hdang29052007 <nguyendang29052007@gmail.com>
2025-12-05 08:51:53 +0700

    first commit

--- src/bai6/ex67/Movable.java
+++ src/bai6/ex67/Movable.java
@@ -1,8 +1,8 @@
 package bai6.ex67;
 
 public interface Movable {
-    void moveUp();
-    void moveDown();
-    void moveLeft();
-    void moveRight();
+    public void moveUp();
+    public void moveDown();
+    public void moveLeft();
+    public void moveRight();
 }
